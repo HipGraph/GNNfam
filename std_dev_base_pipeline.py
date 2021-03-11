338e5f00f4ecbbfe8d45c659aa9ac838f78b76b9
Anuj Godase <abgodase@iu.edu>
2021-03-11 18:39:57 -0500

    averaging test acc

--- std_dev_base_pipeline.py
+++ std_dev_base_pipeline.py
@@ -8,8 +8,24 @@ from torch_geometric.data import Data
 
 import numpy as np
 
-np.random.seed(42)
-torch.manual_seed(42)
+class Net(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, concat=False):
+        super(Net, self).__init__()
+        self.conv1 = SAGEConv(in_channels, 16)
+        self.conv2 = SAGEConv(16, out_channels)
+
+    def forward(self, data):
+        x = F.relu(
+            self.conv1(data.x, data.edge_index)
+        )
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, data.edge_index)
+        return F.log_softmax(x, dim=1)
+
+
+def convert_to_14_bit(num):
+    return list(map(int, list('{0:014b}'.format(num))))
+
 
 startpt = []
 endpt = []
@@ -33,95 +49,77 @@ with open("label.txt", "r") as f:
 print("Done reading")
 num_classes = len(set(labels))
 
+for i in range(-5, 6, 1):
+    seed_val = 42 + i
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
 
-def convert_to_14_bit(num):
-        return list(map(int, list('{0:014b}'.format(num))))
+    edges = torch.tensor([startpt, endpt], dtype=torch.long)
+    y = torch.tensor(labels)
+    y -= 1
+    x = [convert_to_14_bit(val) for val in labels]
+    data = Data(x=torch.Tensor(x), edge_index=edges, y=y)
+    print("Done Prepping data")
 
-edges = torch.tensor([startpt, endpt], dtype=torch.long)
-y = torch.tensor(labels)
-y -= 1
-x = [convert_to_14_bit(val) for val in labels]
-data = Data(x=torch.Tensor(x), edge_index=edges, y=y)
-print("Done Prepping data")
+    nodesize = y.shape[0]
+    trainsize = int(0.8*nodesize)
+    testsize = nodesize - trainsize
 
-nodesize = y.shape[0]
-trainsize = int(0.8*nodesize)
-testsize = nodesize - trainsize
+    edges = torch.tensor([startpt, endpt], dtype=torch.long)
+    y = torch.tensor(labels)
+    y -= 1 
+    x = [convert_to_14_bit(val) for val in labels]
+    data = Data(x=torch.Tensor(x), edge_index=edges, y=y)
+    print("Done Prepping data")
 
-def convert_to_14_bit(num):
-    return list(map(int, list('{0:014b}'.format(num))))
+    nodesize = y.shape[0]
+    trainsize = int(0.8*nodesize)
+    testsize = nodesize - trainsize
+    mask_split = []
 
-edges = torch.tensor([startpt, endpt], dtype=torch.long)
-y = torch.tensor(labels)
-y -= 1 
-x = [convert_to_14_bit(val) for val in labels]
-data = Data(x=torch.Tensor(x), edge_index=edges, y=y)
-print("Done Prepping data")
+    print("Creating train and test masks")
 
-nodesize = y.shape[0]
-trainsize = int(0.8*nodesize)
-testsize = nodesize - trainsize
-mask_split = []
+    with open('mask_split.pkl', 'rb') as f:
+        mask_split = pickle.load(f)
+        mask_split = np.asarray(mask_split)
+        data.train_mask = mask_split == 1
+        data.test_mask = mask_split == 2
 
-print("Creating train and test masks")
+    data.train_mask = torch.from_numpy(data.train_mask)
+    data.test_mask = torch.from_numpy(data.test_mask)
 
-with open('mask_split.pkl', 'rb') as f:
-    mask_split = pickle.load(f)
-    mask_split = np.asarray(mask_split)
-    data.train_mask = mask_split == 1
-    data.test_mask = mask_split == 2
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    in_bits = 14
+    model = Net(in_bits,num_classes).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    data = data.to(device)
 
-data.train_mask = torch.from_numpy(data.train_mask)
-data.test_mask = torch.from_numpy(data.test_mask)
-print(data.train_mask)
-print(data.test_mask)
-
-class Net(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, concat=False):
-        super(Net, self).__init__()
-        self.conv1 = SAGEConv(in_channels, 16)
-        self.conv2 = SAGEConv(16, out_channels)
-
-    def forward(self, data):
-        x = F.relu(
-            self.conv1(data.x, data.edge_index)
-        )
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = self.conv2(x, data.edge_index)
-        return F.log_softmax(x, dim=1)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-in_bits = 14
-model = Net(in_bits,num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
-data = data.to(device)
-
-def train():
-    model.train()
-    total_loss = 0
-    optimizer.zero_grad()
-    out = model(data)
-    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-    loss.backward()
-    optimizer.step()
-    total_loss += loss.item()
-    return total_loss
+    def train():
+        model.train()
+        total_loss = 0
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+        return total_loss
 
 
-def test():
-    model.eval()
-    correct = 0
-    _, pred = model(data).max(dim=1)
-    correct += pred[data.test_mask].eq(data.y[data.test_mask]).sum().item()
-    return correct / data.test_mask.sum().item()
+    def test():
+        model.eval()
+        correct = 0
+        _, pred = model(data).max(dim=1)
+        correct += pred[data.test_mask].eq(data.y[data.test_mask]).sum().item()
+        return correct / data.test_mask.sum().item()
 
-for epoch in range(1, 401):
-    
-    loss = train()
-    test_acc = test()
-    print(
-        'Epoch: {:02d}, Loss: {:.4f}, Test: {:.4f}'.format(
-            epoch, loss, test_acc
-        )
-    )
-
+    print("Training network with seed {}".format(seed_val))
+    total_epochs = 400
+    for epoch in range(1, total_epochs+1):
+        loss = train()
+    print("Trained network for {} epochs".format(total_epochs))
+    data.test_mask[data.test_mask != True] =  True
+    data = data.to(device)
+    test_acc, pred = test()
+    print('Seed Value: {} Test Acc: {:.4f}'.format(seed_val, test_acc))
+    print("----------------------------------------------")
